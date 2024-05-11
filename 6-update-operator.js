92c55c838446fc6927081f6733b9dd670e2aadc8
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-11 08:34:48 +0600

    update operator

--- 6-update-operator.js
+++ 6-update-operator.js
@@ -5,6 +5,17 @@
 db.test.updateOne(
     { _id: ObjectId("6406ad63fc13ae5a40000065") }, // here first, I have to identify the document, that we need to update
     { $set: { age: 105 } } // after finding the document, we can update the document using the $set operator.
+) // [If we use $set, in terms of non-primitive data type and update it with primitive data type, then it will replace the whole array]
+
+//=========================================================//
+
+// $push operator
+
+// Append a value to an array. [Add another value to an array]
+
+db.test.updateOne(
+    { _id: ObjectId("6406ad63fc13ae5a40000065") },
+    { $push: { interests: "Moving" } }
 )
 
 
@@ -12,6 +23,3 @@ db.test.updateOne(
 
 
 
-
-
-//=========================================================//
